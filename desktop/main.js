6c6bf863dc2175420b30875aeaac08d562c3801d
mahadichoixbd-stack <mahadi.choixbd@gmail.com>
2026-09-25 14:18:25 +0600

    Add CHOIX ERP automatic desktop update checks

--- desktop/main.js
+++ desktop/main.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow, Menu, shell } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, Menu, shell, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
 const ERP_URL = 'https://mahadichoixbd-stack.github.io/choix-erp-frontend/';
 
@@ -26,9 +26,7 @@ function createWindow() {
   });
 
   win.webContents.on('did-fail-load', (_event, _code, _description, _validatedURL, isMainFrame) => {
-    if (isMainFrame) {
-      win.loadURL(ERP_URL).catch(() => {});
-    }
+    if (isMainFrame) win.loadURL(ERP_URL).catch(() => {});
   });
 
   const menu = Menu.buildFromTemplate([
@@ -36,6 +34,7 @@ function createWindow() {
       label: 'CHOIX ERP',
       submenu: [
         { label: 'Reload ERP', click: () => win.loadURL(ERP_URL) },
+        { label: 'Check for Updates', click: () => checkForUpdates(true) },
         { type: 'separator' },
         { role: 'quit' }
       ]
@@ -45,8 +44,39 @@ function createWindow() {
   Menu.setApplicationMenu(menu);
 }
 
+async function checkForUpdates(manual = false) {
+  if (!app.isPackaged) return;
+  try {
+    const result = await autoUpdater.checkForUpdates();
+    if (manual && !result?.updateInfo) {
+      await dialog.showMessageBox({ type: 'info', title: 'CHOIX ERP', message: 'You are already using the latest version.' });
+    }
+  } catch (err) {
+    if (manual) {
+      await dialog.showMessageBox({ type: 'warning', title: 'Update check failed', message: 'Could not check for an update right now.' });
+    }
+  }
+}
+
+autoUpdater.autoDownload = true;
+autoUpdater.autoInstallOnAppQuit = true;
+autoUpdater.on('update-available', () => {
+  dialog.showMessageBox({ type: 'info', title: 'CHOIX ERP Update', message: 'A new version is available. It will download automatically in the background.' });
+});
+autoUpdater.on('update-downloaded', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'CHOIX ERP Update Ready',
+    message: 'The update has been downloaded. CHOIX ERP will install it when you close the app.',
+    buttons: ['Restart & Update', 'Later']
+  }).then(({ response }) => {
+    if (response === 0) autoUpdater.quitAndInstall();
+  });
+});
+
 app.whenReady().then(() => {
   createWindow();
+  setTimeout(() => checkForUpdates(false), 5000);
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
